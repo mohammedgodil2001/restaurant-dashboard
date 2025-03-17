277dfed9e2f351ef5372d97551a2146e50d6fd24
mohammedgodil <mohammed.godil@student.howest.be>
2025-03-17 16:42:38 +0100

    deploying the project

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,31 +1,34 @@
 const Menu = ({ menuItems, incrementMenuItem, menu, decrementMenuItem }) => {
     return (
-      <ul className="menu-list">
-        {menuItems.map((item) => {
-          const orderedItem = menu.find((order) => order.id === item.id) || { quantity: 0 };
+        <div >  
+            <span>Menu</span>
+            <ul className="menu-list">
+                {menuItems.map((item) => {
+                const orderedItem = menu.find((order) => order.id === item.id) || { quantity: 0 };
+                
         
-  
-          return (
-            <li key={item.id} className="menu-item">
-                <div className="menu-info">
-                    <div className="menu-image">
-                        <img src={item.image} alt={item.name} />
-                    </div>
-                    <div className="menu-text">
-                        <p className="menu-name">{item.name}</p>
-                        <p className="menu-price">€ {item.price}</p>
-                    </div>
-                </div>
+                return (
+                    <li key={item.id} className="menu-item">
+                        <div className="menu-info">
+                            <div className="menu-image">
+                                <img src={item.image} alt={item.name} />
+                            </div>
+                            <div className="menu-text">
+                                <p className="menu-name">{item.name}</p>
+                                <p className="menu-price">€ {item.price}</p>
+                            </div>
+                        </div>
 
-                <div className="menu-controls">
-                    <button className="decrement-button" onClick={() => decrementMenuItem(item)}><span>-</span></button>
-                    <span className="menu-quantity">{orderedItem.quantity}</span>
-                    <button className="increment-button" onClick={() => incrementMenuItem(item)}>+</button>
-                </div>
-          </li>
-          );
-        })}
-      </ul>
+                        <div className="menu-controls">
+                            <button className="decrement-button" onClick={() => decrementMenuItem(item)}><span>-</span></button>
+                            <span className="menu-quantity">{orderedItem.quantity}</span>
+                            <button className="increment-button" onClick={() => incrementMenuItem(item)}>+</button>
+                        </div>
+                </li>
+                );
+                })}
+            </ul>
+         </div>
     );
   };
   
--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -11,31 +11,36 @@ const Summary = ({item, changeStatus, clicked}) => {
         total -= total * 0.1;
     }
     return(
-        <section className="order-summary">
-            <ul className="summary-list">
-                {item?.map((i) =>(
-                    <li className="summary-list_item" key={i.id}>
-                        <div className="dish-info">
-                            <p className="dish_name">{i.name}</p>
-                            <p className="dish_status">{i.status === "preparing" ? i.status + "..." : i.status}</p>
-                            <p className="dish_quantity">{i.quantity}</p>
-                        </div>
-                        <div className="dish-price">
-                            <p>€ {i.price}</p>
-                            <button  onClick={() => changeStatus(i.id)}>{i.status === "preparing" ? "Ready" : "Served"}</button>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-            <div>
-            {item?.length > 0 && (
-            <div className="pricing-summary">
-                {clicked && <p className="total-before-discount">Total before discount <span>{"€ "  + totaling}</span></p>}
-                <p className="final-total">{clicked ? 'Bill after discount' : 'Total'} <span>{"€ "  +total}</span></p>
-            </div>
-            )}
-            </div>
-        </section>
+        <div className="summary-wrapper">
+            <h2>Order Summary</h2>
+            <section className="order-summary">
+                <h3 className="visually-hidden">Ordered items</h3>
+                <ul className="summary-list">
+                    {item?.map((i) =>(
+                        <li className="summary-list_item" key={i.id}>
+                            <div className="dish-info">
+                                <p className="dish_name">{i.name}</p>
+                                <p className="dish_status">{i.status === "preparing" ? i.status + "..." : i.status}</p>
+                                <p className="dish_quantity">{i.quantity}</p>
+                            </div>
+                            <div className="dish-price">
+                                <p>€ {i.price}</p>
+                                <button  onClick={() => changeStatus(i.id)}>{i.status === "preparing" ? "Ready" : "Served"}</button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                <div>
+                {item?.length > 0 && (
+                <div className="pricing-summary">
+                    {clicked && <p className="total-before-discount">Total before discount <span>{"€ "  + totaling}</span></p>}
+                    <p className="final-total">{clicked ? 'Bill after discount' : 'Total'} <span>{"€ "  +total}</span></p>
+                </div>
+                )}
+                </div>
+            </section>
+        
+        </div>
     )
   };
   
